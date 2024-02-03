a47612eb3967eb7bb05f748e79546340df7b844e
Yan Monteiro de Melo <ymm@ecomp.poli.br>
2024-02-03 13:32:14 -0300

    Update PythonSheets.py
    Ajustes no código para que reconheça e funcione independentemente da quantidade de linhas presentes na planilha.

--- PythonSheets.py
+++ PythonSheets.py
@@ -8,7 +8,7 @@ import logging
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-# Configurar o nível de log conforme necessário (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+# Set the log level as needed (DEBUG, INFO, WARNING, ERROR, CRITICAL)
 logging.basicConfig(level=logging.INFO)
 
 def authenticate_google_sheets():
@@ -46,53 +46,67 @@ def main():
         
         add_values = []
         # List to store information and update in the spreadsheet at the end
+        result = (
+            sheet.values()
+            .get(spreadsheetId="1nyEeA-tUxuHUquDWbo3iH8ZY1Jh3xlDV4WSgZ5mDZZk", range="engenharia_de_software!A:A")
+            .execute()
+        )
+        total_rows = result.get("values", [])
         
-        for row in range(4, 28):
-            result_score = (
-                sheet.values()
-                .get(spreadsheetId="1nyEeA-tUxuHUquDWbo3iH8ZY1Jh3xlDV4WSgZ5mDZZk", range=f"engenharia_de_software!D{row}:F{row}")
-                .execute()
-            )
-            result_absence = (
-                sheet.values()
-                .get(spreadsheetId="1nyEeA-tUxuHUquDWbo3iH8ZY1Jh3xlDV4WSgZ5mDZZk", range=f"engenharia_de_software!C{row}")
-                .execute()
-            )
+        # Check if there is at least one row in the spreadsheet
+        if total_rows:
+            total_rows = len(total_rows)
             
-            absence = int(result_absence.get("values", [["0"]])[0][0])
-            # Get the absence values present in the spreadsheet
+            # Use all available rows
+            for row in range(4, total_rows + 1):
+              result_score = (
+                  sheet.values()
+                  .get(spreadsheetId="1nyEeA-tUxuHUquDWbo3iH8ZY1Jh3xlDV4WSgZ5mDZZk", range=f"engenharia_de_software!D{row}:F{row}")
+                  .execute()
+              )
+              result_absence = (
+                  sheet.values()
+                  .get(spreadsheetId="1nyEeA-tUxuHUquDWbo3iH8ZY1Jh3xlDV4WSgZ5mDZZk", range=f"engenharia_de_software!C{row}")
+                  .execute()
+              )
+              
+              absence = int(result_absence.get("values", [["0"]])[0][0])
+              # Get the absence values present in the spreadsheet
+              
+              score = result_score.get("values", [["", "", ""]])[0]
+              score_float = [float(value) if value else 0 for value in score]
+              # Get the grades' values present in the spreadsheet and transform them into FLOAT
+              
+              average = sum(score_float) / len(score_float)
+              # Calculate the average
+              
+              # Log the average
+              logging.info(f"Average for row {row}: {average}")
+              
+              # Evaluate the situation and calculate passing grade
+              if absence > 15:
+                  situation = "Reprovado por Falta"
+              elif average < 50:
+                  situation = "Reprovado por Nota"
+              elif 50 <= average < 70:
+                  situation = "Exame Final"
+              else:
+                  situation = "Aprovado"
+              
+              passing_grade = calculate_passing_grade(average, situation)
+              add_values.append([situation, passing_grade])
+        else:
+            logging.warning("The spreadsheet is empty.")
             
-            score = result_score.get("values", [["", "", ""]])[0]
-            score_float = [float(value) if value else 0 for value in score]
-            # Get the grades' values present in the spreadsheet and transform them into FLOAT
-            
-            average = sum(score_float) / len(score_float)
-            # Calculate the average
-            
-            # Log the average
-            logging.info(f"Average for row {row}: {average}")
-            
-            # Evaluate the situation and calculate passing grade
-            if absence > 15:
-                situation = "Reprovado por Falta"
-            elif average < 50:
-                situation = "Reprovado por Nota"
-            elif 50 <= average < 70:
-                situation = "Exame Final"
-            else:
-                situation = "Aprovado"
-            
-            passing_grade = calculate_passing_grade(average, situation)
-            add_values.append([situation, passing_grade])
-        
         # Log the values to be added
         logging.info(f"Values to be added: {add_values}")
         
+        update_range = f"engenharia_de_software!G4:H{total_rows + 3}"
         result = (
             sheet.values()
             .update(
                 spreadsheetId="1nyEeA-tUxuHUquDWbo3iH8ZY1Jh3xlDV4WSgZ5mDZZk",
-                range="engenharia_de_software!G4:H27",
+                range=update_range,
                 valueInputOption="USER_ENTERED",
                 body={"values": add_values},
             )
